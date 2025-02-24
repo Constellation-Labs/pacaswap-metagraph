1c90cca033c6486b73156bd901c86fd3c5a8473b
Marcus Vinicius Girolneto Sousa <marcus.girolneto@gmail.com>
2025-02-24 13:23:45 -0300

    refactor: refactoring governance CI tests to use `retry` function

---
+++ .github/action_scripts/governance/token-lock/index.ts
@@ -0,0 +1,180 @@
+import { dag4 } from "@stardust-collective/dag4";
+import jsSha256 from "js-sha256";
+import axios from "axios";
+import { BaseAmmMetagraphCliArgsSchema, delay, getPublicKey, log, retry, serializeBrotli } from "../../shared";
+import { z } from 'zod';
+
+const tokenLocks = [
+    {
+        "privateKey": "8971dcc9a07f2db7fa769582139768fd5d73c56501113472977eca6200c679c8",
+        "lockAmount": 1000,
+        "multiplier": 0.25,
+        "expireEpochProgress": 5000
+    }, {
+        "privateKey": "7bf1b37351f164ae4cdc5f10a55987163d42bf68e607383e7fa5121f7e7624ed",
+        "lockAmount": 1000,
+        "multiplier": 0.25,
+        "expireEpochProgress": 5000
+    }]
+
+const TokenLockSchema = z.object({
+    privateKey: z.string()
+        .min(1, "Private key cannot be empty")
+        .nullish()
+        .refine(val => val != null, {
+            message: "privateKey is required"
+        }),
+    lockAmount: z.number()
+        .nullish()
+        .refine(val => val != null, {
+            message: "lockAmount is required"
+        }),
+    multiplier: z.number()
+        .nullish()
+        .refine(val => val != null, {
+            message: "multiplier is required"
+        }),
+    expireEpochProgress: z.number()
+        .nullish()
+        .refine(val => val != null, {
+            message: "expireEpochProgress is required"
+        })
+});
+
+const createConfig = (argsObject: object) => {
+    const argsObj = {
+        ...argsObject,
+        tokenLocks
+    }
+
+    const CliArgsSchema = BaseAmmMetagraphCliArgsSchema.extend({
+        tokenLocks: z.array(TokenLockSchema)
+    });
+
+    return CliArgsSchema.parse(argsObj);
+};
+
+
+const getSignedTokenLock = async (config, { privateKey, publicKey, address, lockAmount, expireEpochProgress }) => {
+    log("Generating signed token locks...");
+    const { metagraphId, ammCl1Url } = config;
+
+    log(`Fetching last reference for wallet: ${address}`);
+    const { data: lastRef } = await axios.get(`${ammCl1Url}/token-locks/last-reference/${address}`);
+    const { hash, ordinal } = lastRef;
+
+    const body = {
+        amount: lockAmount,
+        currencyId: metagraphId,
+        parent: { hash, ordinal },
+        source: address,
+        unlockEpoch: expireEpochProgress,
+    };
+
+    const serializedTx = await serializeBrotli(body);
+    const messageHash = jsSha256.sha256(Buffer.from(serializedTx, "hex"));
+    const signature = await dag4.keyStore.sign(privateKey, messageHash);
+
+    log(`Signed token lock generated for wallet: ${address}`);
+
+    return {
+        value: body,
+        proofs: [{ id: publicKey, signature }],
+    };
+};
+
+const sendSignedTokenLock = async (config, signedTokenLock) => {
+    const { ammCl1Url } = config;
+    try {
+        log("Sending signed token lock...");
+        await axios.post(`${ammCl1Url}/token-locks`, signedTokenLock);
+        log("Signed token lock sent successfully.");
+    } catch (error) {
+        log(`Error sending signed token lock: ${error.message}`, "ERROR");
+        throw error;
+    }
+};
+
+const validateTokenLockInGL0 = async (
+    config: ReturnType<typeof createConfig>,
+    walletAddress: string,
+    logger: (message: string, type?: string, context?: string) => void = log
+) => {
+    const { gl0Url, metagraphId } = config;
+
+    try {
+
+        const { data: snapshot } = await axios.get(`${gl0Url}/global-snapshots/latest/combined`);
+        const tokenLockBalances = snapshot[1]?.tokenLockBalances;
+
+        if (tokenLockBalances?.[metagraphId]?.[walletAddress]) {
+            logger("Token lock successfully accepted in GL0.");
+            return;
+        }
+    } catch (error) {
+        logger(`Error checking token lock in GL0: ${error.message}`, "ERROR");
+        throw error
+    }
+};
+
+const validateVotingWeight = async (
+    config: ReturnType<typeof createConfig>,
+    address: string,
+    lockAmount: number,
+    multiplier: number,
+    logger: (message: string, type?: string, context?: string) => void = log
+) => {
+    const { ammMl0Url } = config;
+    const expectedWeight = lockAmount * multiplier;
+    try {
+        const { data: state } = await axios.get(`${ammMl0Url}/v1/addresses/${address}/vote-weight`);
+        const actualWeight = state.total;
+        if (actualWeight === expectedWeight) {
+            logger(`Voting weight validated for ${address}: ${actualWeight}`);
+            return;
+        }
+        logger(`Voting weight mismatch for ${address}. Retrying...`);
+    } catch (error) {
+        logger(`Error validating voting weight: ${error.message}`, "ERROR");
+        throw error
+    }
+};
+
+const tokenLockTests = async (argsObject: object) => {
+    const config = createConfig(argsObject)
+
+    log("Building token locks information...");
+    const tokenLocksInfo = config.tokenLocks.map((data) => {
+        const account = dag4.createAccount();
+        account.loginPrivateKey(data.privateKey);
+
+        return {
+            privateKey: data.privateKey,
+            publicKey: getPublicKey(account),
+            address: account.address,
+            lockAmount: data.lockAmount,
+            multiplier: data.multiplier,
+            expireEpochProgress: data.expireEpochProgress,
+        };
+    });
+    log("Token lock information built successfully.");
+
+    for (const tokenLockInfo of tokenLocksInfo) {
+        const { address, lockAmount, multiplier } = tokenLockInfo;
+
+        const signedTokenLock = await getSignedTokenLock(config, tokenLockInfo);
+        await sendSignedTokenLock(config, signedTokenLock);
+        await retry('Validate token lock in GL0')(async (logger) => {
+            await validateTokenLockInGL0(config, address, logger);
+        })
+        await retry('Validate voting weight')(async (logger) => {
+            await validateVotingWeight(config, address, lockAmount, multiplier, logger);
+        })
+
+        await delay(10000)
+    }
+
+    log("All token locks validated successfully.");
+};
+
+export { tokenLockTests }
---
+++ .github/action_scripts/governance/vote-allocation/index.ts
@@ -0,0 +1,169 @@
+import { dag4 } from "@stardust-collective/dag4";
+import axios from "axios";
+import { delay, getPublicKey, log, retry, BaseAmmMetagraphCliArgsSchema, serializeBase64 } from "../../shared";
+import { z } from 'zod';
+
+const voteAllocations = [{
+    "privateKey": "8971dcc9a07f2db7fa769582139768fd5d73c56501113472977eca6200c679c8",
+    "allocations": [{ "key": "NodeValidators", "amount": 10 }]
+}]
+
+const AllocationsSchema = z.object({
+    key: z.string()
+        .min(1, "key cannot be empty"),
+    amount: z.number()
+        .min(1, "amount must be greater than 0"),
+});
+
+const VoteAllocationSchema = z.object({
+    privateKey: z.string()
+        .min(1, "Private key cannot be empty"),
+    allocations: z.array(AllocationsSchema)
+        .min(1, "Allocations array cannot be empty"),
+});
+
+const createConfig = (argsObject: object) => {
+    const argsObj = {
+        ...argsObject,
+        voteAllocations
+    }
+
+    const CliArgsSchema = BaseAmmMetagraphCliArgsSchema.extend({
+        voteAllocations: z.array(VoteAllocationSchema)
+    });
+
+    return CliArgsSchema.parse(argsObj);
+};
+
+const getSignedVoteAllocation = async (config, { privateKey, publicKey, address, allocations }) => {
+    log("Generating signed vote allocations...");
+    const { ammMl0Url } = config;
+
+    log(`Fetching last reference for wallet: ${address} ${`${ammMl0Url}/v1/addresses/${address}/vote-info/last-reference`}`);
+    const { data: lastRef } = await axios.get(`${ammMl0Url}/v1/addresses/${address}/vote-info/last-reference`);
+    const { hash, ordinal } = lastRef;
+    const parsedAllocations = allocations.map(allocation => [allocation.key, allocation.amount])
+
+    const body = {
+        RewardAllocationVoteUpdate: {
+            address,
+            allocations: parsedAllocations,
+            parent: {
+                hash,
+                ordinal
+            }
+        },
+    };
+
+    const encodedMessage = await serializeBase64(body)
+    const signature = await dag4.keyStore.dataSign(
+        privateKey,
+        encodedMessage
+    );
+
+    log(`Signed vote allocation generated for wallet: ${address}`);
+
+    return {
+        value: body,
+        proofs: [{ id: publicKey, signature }],
+    };
+};
+
+const sendSignedVoteAllocation = async (config, signedVoteAllocation) => {
+    const { ammDl1Url } = config;
+    try {
+        log("Sending signed vote allocation...");
+        await axios.post(`${ammDl1Url}/data`, signedVoteAllocation);
+        log("Signed vote allocation sent successfully.");
+    } catch (error) {
+        log(`Error sending signed vote allocation: ${error.message}`, "ERROR");
+        throw error;
+    }
+};
+
+
+const validateVoteAllocations = async (
+    config: ReturnType<typeof createConfig>,
+    address: string,
+    logger: (message: string, type?: string, context?: string) => void = log
+) => {
+    const { ammMl0Url } = config;
+
+    try {
+        await axios.get(`${ammMl0Url}/v1/addresses/${address}/vote-info`);
+        logger(`Vote info filled`);
+        return
+    } catch (error) {
+        logger(`Error validating vote allocation: ${error.message}`, "WARN");
+        throw error
+    }
+
+};
+
+const validateAllocationsRewards = async (
+    config: ReturnType<typeof createConfig>,
+    logger: (message: string, type?: string, context?: string) => void = log
+) => {
+    const { ammMl0Url } = config;
+    try {
+        const { data: lastRewards } = await axios.get(`${ammMl0Url}/v1/governance/allocations/rewards`);
+
+        const someRewardFilled = lastRewards.find(reward => {
+            return Object.keys(reward.rewardsInfo).length > 0
+        })
+
+        if (someRewardFilled) {
+            const nodeValidatorsReward = someRewardFilled.rewardsInfo.NodeValidators
+            if (nodeValidatorsReward === 250) {
+                logger(`Rewards filled correctly`);
+                return
+            }
+            throw new Error(`NodeValidatorsReward invalid weight: ${nodeValidatorsReward}`);
+        }
+
+        logger(`Rewards not filled yet. Retrying...`);
+    } catch (error) {
+        logger(`Error checking allocation rewards: ${error.message}`, "WARN");
+    }
+};
+
+const voteAllocationTests = async (argsObject: object) => {
+    const config = createConfig(argsObject)
+
+    log("Building vote allocations information...");
+    const voteAllocationsInfo = config.voteAllocations.map((data) => {
+        const account = dag4.createAccount();
+        account.loginPrivateKey(data.privateKey);
+
+        return {
+            privateKey: data.privateKey,
+            publicKey: getPublicKey(account),
+            address: account.address,
+            allocations: data.allocations,
+        };
+    });
+
+    log("Vote allocation information built successfully.");
+
+    for (const voteAllocationInfo of voteAllocationsInfo) {
+        const { address, _ } = voteAllocationInfo;
+
+        const signedVoteAllocation = await getSignedVoteAllocation(config, voteAllocationInfo);
+        await sendSignedVoteAllocation(config, signedVoteAllocation);
+        await retry('Validate voting weight')(async (logger) => {
+            await validateVoteAllocations(config, address, logger)
+        })
+        await delay(10000)
+    }
+
+    log(`Waiting 2 minutes to check if the allocations were clear and the rewards filled`)
+    await delay(120 * 1000)
+
+    await retry('Validate allocations rewards')(async (logger) => {
+        await validateAllocationsRewards(config, logger)
+    })
+
+    log("All vote allocations validated successfully.");
+};
+
+export { voteAllocationTests }
