06eb619a356f79e4f7f1090638d6e8fd7711f04a
Marcus Vinicius Girolneto Sousa <marcus.girolneto@gmail.com>
2025-02-24 12:24:56 -0300

    refactor: refactoring directories in amm CI tests

---
+++ .github/action_scripts/amm-operations/main.ts
@@ -0,0 +1,53 @@
+import { log } from '../shared';
+import { liquidityPoolTests } from './liquidity-pool'
+
+const getArgs = () => {
+  const args = process.argv.slice(2);
+
+  const [
+    gl0Url,
+    dagCl1Url,
+    ammMl0Url,
+    ammCl1Url,
+    ammDl1Url,
+    tokenACl1Url,
+    tokenBCl1Url,
+    tokenAMl0Url,
+    tokenBMl0Url,
+    ammMetagraphId,
+    tokenAId,
+    tokenBId,
+  ] = args;
+
+  if (args.length < 11) {
+    throw new Error(
+      "Usage: npx tsx amm-operations/main.ts <gl0-url> <dagcl1-url> <aml0-url> <acl1-url> <adl1-url> <tacl1-url> <tbcl1-url> <taml0-url> <tbml0-url> <amm-metagraph-id> <tokenAId> <tokenBId>"
+    );
+  }
+
+  return {
+    gl0Url,
+    dagCl1Url,
+    ammMl0Url,
+    ammCl1Url,
+    ammDl1Url,
+    tokenACl1Url,
+    tokenBCl1Url,
+    tokenAMl0Url,
+    tokenBMl0Url,
+    ammMetagraphId,
+    tokenAId,
+    tokenBId
+  }
+};
+
+const main = async () => {
+  const args = getArgs();
+
+  await liquidityPoolTests(args);
+};
+
+main().catch((error) => {
+  log(`Error: ${error.message}`, "ERROR");
+  process.exit(1);
+});
