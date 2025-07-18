00c1f493e9c3c5210b5f676fcf6afe84d8cc3279
Oleg Aleksandrov <oleg.v.aleksandrov@gmail.com>
2025-07-18 13:58:26 +0100

    feat: add governance allocation stats

--- modules/l0/src/main/scala/org/amm_metagraph/l0/custom_routes/GovernanceRoutes.scala
+++ modules/l0/src/main/scala/org/amm_metagraph/l0/custom_routes/GovernanceRoutes.scala
@@ -3,6 +3,8 @@ package org.amm_metagraph.l0.custom_routes
 import cats.effect.Async
 import cats.syntax.all._
 
+import derevo.circe.magnolia.{decoder, encoder}
+import derevo.derive
 import io.circe.generic.auto._
 import org.amm_metagraph.l0.custom_routes.Responses.SingleResponse
 import org.amm_metagraph.shared_data.calculated_state.CalculatedStateService
@@ -18,7 +20,25 @@ case class GovernanceRoutes[F[_]: Async](
       Ok(SingleResponse(calculatedState.state.allocations.frozenUsedUserVotes))
     }
 
+  private def getAllocationStats: F[Response[F]] =
+    calculatedStateService.get.flatMap { calculatedState =>
+      val totalVpIssued = calculatedState.state.votingWeights.values.map(v => BigDecimal(v.total.value)).sum
+
+      val allocatedAddresses = calculatedState.state.votingWeights.keySet -- calculatedState.state.allocations.usersAllocations.keySet
+      val totalVpAllocated =
+        calculatedState.state.votingWeights.filter(vw => allocatedAddresses.contains(vw._1)).values.map(v => BigDecimal(v.total.value)).sum
+
+      val totalTokenLocked =
+        calculatedState.state.votingWeights.flatMap(_._2.info).map(vw => BigDecimal(vw.tokenLock.amount.value.value)).sum
+
+      Ok(AllocationStats(totalVpIssued, totalVpAllocated, totalTokenLocked))
+    }
+
   val routes: HttpRoutes[F] = HttpRoutes.of[F] {
     case GET -> Root / "governance" / "allocations" / "rewards" => getAllocationsRewards
+    case GET -> Root / "governance" / "stats"                   => getAllocationStats
   }
 }
+
+@derive(encoder, decoder)
+case class AllocationStats(totalVpIssued: BigDecimal, totalVpAllocated: BigDecimal, totalTokenLocked: BigDecimal)
