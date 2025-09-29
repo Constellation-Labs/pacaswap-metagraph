4ef2cac55831240fa10d03e162d90903b4e84762
Marcus Vinicius Girolneto Sousa <marcus.girolneto@gmail.com>
2025-09-29 11:38:39 -0300

    fix: updating tessellation version

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -3,7 +3,7 @@ import sbt.*
 object Dependencies {
 
   object V {
-    val tessellation = "3.4.10"
+    val tessellation = "3.4.0-rc.13"
     val decline = "2.4.1"
     val organizeImports = "0.5.0"
   }
